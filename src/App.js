58ce8f3e78dd24dccad4fa9145dbd2e899ebd906
leigharobinson <lmarobinson24@gmail.com>
2020-10-26 13:42:26 -0500

    made some simple circles using d3 and React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useEffect, useState} from "react";
+// import logo from './logo.svg';
+import { select } from "d3";
+import "./App.css";
+
+
 
 function App() {
+  const [data, setData] = useState([25,30,45,60,20])
+  const svgRef = useRef();
+  // console.log(svgRef);
+
+  useEffect(()=> {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    // console.log(svg.selectAll("circle").data(data))
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+      enter => enter.append("circle"),
+      update => update.attr("class", "updated"),
+      exit => exit.remove()
+    ).attr("r", value => value)
+    .attr("cx", value => value *2)
+    .attr("cy", value => value * 2)
+    .attr("stroke", "red");
+  }, [data])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Hello D3 & React</h1>
+      {/* <svg>
+        {data.map(value => <circle r={value}></circle>)}
+      </svg> */}
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={()=> setData(data.map(value => value +5))}>
+        Update data
+      </button>
+      <button onClick={()=> setData(data.filter(value => value <= 35))}>
+        Filter data
+      </button>
+      
+    </React.Fragment>
   );
 }
 
